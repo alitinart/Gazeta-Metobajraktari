05849da6bb7887667501743f64680161a2b67ff2
Nart Aliti <76398544+alitinart@users.noreply.github.com>
2022-03-24 19:24:12 +0100

    Article Change

--- src/database/models/article.model.js
+++ src/database/models/article.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Article = new mongoose.Schema({
   title: { type: String, require: "This field is required" },
   text: { type: String, require: "This field is required" },
+  summary: { type: String, require: "This field is required" },
   cover: { type: String, require: "This field is required" },
   authorId: { type: String, require: "This field is requrired" },
 });

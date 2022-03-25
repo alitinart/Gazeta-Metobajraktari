60ea3bf295467dae18de4a7c38cfdfcd0ad8f3fe
Nart Aliti <76398544+alitinart@users.noreply.github.com>
2022-03-25 16:15:16 +0100

    Meta

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -39,6 +39,7 @@ export default function Article({ article, articles }) {
     <>
       <Head>
         <title>{article.title}</title>
+        <meta name="description" content={article.title}></meta>
       </Head>
       <div className="main">
         <div className="latest-article article-content">
